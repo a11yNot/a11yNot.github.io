6a59141896906f0b261bef4f226911e69ead190a
garcialo <github@garcialo.com>
2015-10-08 01:33:00 -0500

    adding role = presentation to gist layout tables

--- includes/js/gist.js
+++ includes/js/gist.js
@@ -1,5 +1,5 @@
-// replacing unicode character u2764 (heart) with an "image" until assistive technology supports unicode characters
 window.onload = function() {
+	// replacing unicode character u2764 (heart) with an "image" until assistive technology supports unicode characters
 	var listOfGistMetas = document.getElementsByClassName("gist-meta");
 
 	for (var i=0; i < listOfGistMetas.length; i++) {
@@ -7,4 +7,14 @@ window.onload = function() {
 		var changedString = originalString.replace(' \u2764 ', " <span role='img' aria-label='love'>\u2764</span> ");
 		listOfGistMetas[i].innerHTML = changedString;
 	}
+	
+	// adding role="presentation" to the embedded gist's layout table
+	var listOfGistTables = document.querySelectorAll("table.js-file-line-container");
+	
+	for (var i=0; i < listOfGistTables.length; i++) {
+		var originalTable = listOfGistTables[i];
+		originalTable.setAttribute("role","presentation");
+	}
 }
+
+
